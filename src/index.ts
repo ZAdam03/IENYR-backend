ba4511535487e127cea9e3bcd46ece637c748710
ZAdam03 <zamboadamzoltan@gmail.com>
2025-03-22 21:42:10 +0100

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
+import apiGroup from "./groups/api";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  app
+  .use(apiGroup)
+  
+  .onError(({ code }) => {
+    if (code === 'NOT_FOUND') {
+        return 'Route not found :('
+    }
+  })
+  .listen(process.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
